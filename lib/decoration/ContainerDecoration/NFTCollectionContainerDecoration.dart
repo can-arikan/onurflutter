35973677d3118ad32c11c7a39bfb3e31296d4306
Can ARIKAN <can.arikan@valensas.com>
2023-04-16 21:01:51 +0300

    Delete Everything

--- lib/decoration/ContainerDecoration/NFTCollectionContainerDecoration.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-
-const containerColor = Color(0x2CFFFFFF);
-var containerBoxDecoration = BoxDecoration(
-  color: containerColor,
-  borderRadius: BorderRadius.all(Radius.circular(10))
-
-
-);
